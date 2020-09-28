5b2d3da3f9aac37916f3ba656837bb0bfd16c436
vsteere <vssteere@gmail.com>
2020-09-28 14:03:49 -0500

    set up orm.js and connection.js files

---
+++ config/orm.js
@@ -0,0 +1,54 @@
+const connection = require("../config/connection.js");
+
+
+//helper functions from a previous activity
+
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+
+function objToSql(ob) {
+    let arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+};
+
+let orm = {
+
+selectall: function() {
+
+
+},
+
+insertOne: function() {
+
+
+
+},
+
+updateOne: function() {
+
+
+}
+
+}
