ee04ee232a879375dc7d5e992c75e8c114a849bc
vsteere <vssteere@gmail.com>
2020-09-28 13:53:06 -0500

    set up and execute SQL files and set up connection.js

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(30) UNIQUE NOT NULL,
+  devoured BOOLEAN, 
+  PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", false), ("mushroom swiss burger", false), ("prime rib brisket BBQ burger", false);
