a5f79b38bb552c7d366c3d54f5d8a9212e0bd948
vsteere <vssteere@gmail.com>
2020-09-29 13:24:42 -0500

    server file data

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//be able to use the files out of the public directory
+app.use(express.static("public"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// bring in Express Handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Starts the server
+app.listen(PORT, function() {
+  
+  console.log("Server listening on: http://localhost:" + PORT);
+});
