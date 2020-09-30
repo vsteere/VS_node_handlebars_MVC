ebf6463baf46a0ea1d7d4c8052172770324f3cb6
vsteere <vssteere@gmail.com>
2020-09-30 17:42:32 -0500

    more connection work

--- config/connection.js
+++ config/connection.js
@@ -23,11 +23,16 @@ else {
     database: "burgers_db"
   });
 
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
 
 }
-
-
-
 
 connection.connect(function(err) {
   if (err) {
